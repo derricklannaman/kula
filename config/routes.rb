666220339b85093de61ba2baf99664e89c6d0769
Derrick Lannaman <derricklannaman@gmail.com>
2016-04-09 17:22:55 -0500

    add destination spec, and routes etc

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
 
   root 'home#index'
-
+  get 'destinations', to: 'destinations#index'
 end
--- spec/features/visiting_pages_spec.rb
+++ spec/features/visiting_pages_spec.rb
@@ -1,8 +1,16 @@
 require "rails_helper"
 
-RSpec.feature "Users can visit pages" do
+RSpec.feature "Users can visit" do
   scenario "the index page" do
     visit "/"
-    expect(page).to have_content("Kula")
+    expect(page).to have_content("KULA")
   end
+
+  it 'allows a user to visit the destination page' do
+    visit "/"
+    click_link('Destinations')
+    expect(current_path).to eq("/destinations")
+    expect(page).to have_content('destinations')
+  end
+
 end

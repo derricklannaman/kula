bf6f946874e0836cbc49d641c18b47f1dbd2cc2b
Derrick Lannaman <derricklannaman@gmail.com>
2016-05-14 15:28:10 -0500

    first pass at side nav destination functionality

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,2 +1,11 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).ready(function(){
+  $('ul.location-list').hide();
+  $('a.icon.icon-destinations').on('click', revealLocations);
+
+});
+
+function revealLocations() {
+  $('ul.location-list').slideDown('slow');
+}

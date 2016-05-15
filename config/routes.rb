375f5376686064377d8ee11cde185a7b706c6a46
Derrick Lannaman <derricklannaman@gmail.com>
2016-05-14 20:59:27 -0500

    tightening up side-nav transitions

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'destinations', to: 'destinations#index'
   get 'discover/jamaica', to: 'destinations#jamaica'
+  get 'discover/cuba', to: 'destinations#jamaica'
+  get 'discover/brazil', to: 'destinations#jamaica'
 end
